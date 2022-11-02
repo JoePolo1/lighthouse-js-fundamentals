8820d715383129c84de60242d63efe23f5dc0910
Joe Polo <joe.polo@telus.com>
2022-11-02 15:14:31 -0700

    added test javascript file

---
+++ test.js
@@ -0,0 +1,8 @@
+const whichSchool  = 12; //sets the variable whichSchool based on the age function
+const school1 = "Elementary School"; // The next 3 lines define the different school options as variables
+const school2 = "Secondary School";
+const school3 = "Lighthouse Labs";
+
+  if (whichSchool > 13) {
+    console.log("Since the student is " + whichSchool + " years old, they should attend " + school1 + ".")
+  }
