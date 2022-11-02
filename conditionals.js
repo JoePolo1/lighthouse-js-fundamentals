fc0dca055aecf41c1a5e7fedaa088debb8aca7b5
Joe Polo <joe.polo@telus.com>
2022-11-01 23:59:07 -0700

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -1,13 +1,20 @@
-const temperature = 24;
+const temperature = 40;
+const raining = false;
 
 if (temperature < 0) {
   console.log("Make sure you pick out a scarf!");
 } else if (temperature < 15) {
   console.log("Short sleeves won't cut it!");
+} else if (temperature < -35 || temperature > 35) {
+  console.log("Extreme temperature alert! Consider staying indoors.");
 } else if (temperature >= 25) {
   console.log("Bring water and sunblock, it is hot!");
+
 } else {
   console.log("Short sleeves are fine.");
 }
 
-console.log("Now you're ready to go outside!");
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
+console.log("Now you know about the weather, back to you, anchorman!");
