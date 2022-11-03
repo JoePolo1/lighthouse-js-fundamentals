f0b623afcaa893ccd34495c68fd2ae8e882eb893
Joe Polo <joe.polo@telus.com>
2022-11-03 10:38:15 -0700

    adding age calculator using functions, strings return and console prints

---
+++ ageCalculator.js
@@ -0,0 +1,8 @@
+let name = "Joe";
+let yearOfBirth = 1986;
+let currentYear = 2022;
+
+function ageCalculator(name, yearOfBirth, currentYear) {
+  return(name + " is " + (currentYear - yearOfBirth) + " years old.");
+}
+console.log(ageCalculator("Miranda", 1983, 2022));
