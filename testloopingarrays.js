a7bf0f694c7a338dd08915c9b126071816f7a00f
Joe Polo <joe.polo@telus.com>
2022-11-02 20:14:18 -0700

    commit all

---
+++ testloopingarrays.js
@@ -0,0 +1,11 @@
+var sound = "" ;
+
+var laugh = function(num) {
+    for(var x = 0; x < num; x++) {
+        sound = sound = "ha" ;
+    }
+    sound = sound +"!";
+    return sound;
+}
+
+console.log(laugh(10));
