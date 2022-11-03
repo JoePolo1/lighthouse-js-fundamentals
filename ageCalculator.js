3525ae7ee8260f8387486020f4561a37d284cdc9
Joe Polo <joe.polo@telus.com>
2022-11-03 10:40:03 -0700

    updated age calculator with commented out optional use of template literals to show other successful examples

--- ageCalculator.js
+++ ageCalculator.js
@@ -6,3 +6,11 @@ function ageCalculator(name, yearOfBirth, currentYear) {
   return(name + " is " + (currentYear - yearOfBirth) + " years old.");
 }
 console.log(ageCalculator("Miranda", 1983, 2022));
+
+/* Optional alternative using TEMPLATE LITERALS:
+
+const ageCalculator = (name, yearOfBirth, currentYear) => {
+  return (`${name} is ${currentYear - yearOfBirth} years old.`);
+}
+
+*/
