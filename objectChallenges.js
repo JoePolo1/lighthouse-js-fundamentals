312f67e7b24df35cf14bd79afda8b06a7d8b186d
Joe Polo <joe.polo@telus.com>
2022-11-04 18:11:45 -0700

    adding some challenge notes and files related to objects and functions for later review

--- objectChallenges.js
+++ objectChallenges.js
@@ -1,4 +1,28 @@
 /*
+Our function will receive two arguments:
+
+The first argument, trash, is a string that will tell our function what type of item is being submitted.
+The second argument, bins, is an object containing three properties (waste, recycling, and compost), 
+which hold some numerical value. Our function must increase the correct value in the bins object, 
+and then return the newly updated object.
+*/
+
+const smartGarbage = function (trash, bins) {
+  bins[trash] += 1;
+  return bins;
+}
+smartGarbage('recycling', { waste: 4, recycling: 2, compost: 5 });
+
+
+//Should return the object 
+/* {
+  waste: 4,
+  recycling: 3,
+  compost: 5
+}
+*/
+
+/*
 The function, carPassing(cars, speed), takes in an array of car objects, and the speed 
 of a car as it passes the sensor. This function should create a new object with a property 
 called speed, and another property called time and add it to the cars array. 
